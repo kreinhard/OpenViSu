d98992445ec28b9331c426b818e2a2351d8fea5f
Kai Reinhard <K.Reinhard@micromata.de>
2016-04-03 22:03:10 +0200

    Security and Cross-Site-Request-Forgery protection.

--- src/main/java/org/openvisu/OpenVisuApplication.java
+++ src/main/java/org/openvisu/OpenVisuApplication.java
@@ -5,13 +5,12 @@ import java.util.HashMap;
 import java.util.Map;
 import java.util.UUID;
 
+import org.openvisu.security.SecurityConfiguration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.SecurityProperties;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.annotation.Order;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -21,14 +20,8 @@ public class OpenVisuApplication
 {
   @Configuration
   @Order(SecurityProperties.ACCESS_OVERRIDE_ORDER)
-  protected static class SecurityConfiguration extends WebSecurityConfigurerAdapter
+  protected static class MySecurityConfiguration extends SecurityConfiguration
   {
-    @Override
-    protected void configure(HttpSecurity http) throws Exception
-    {
-      http.httpBasic().and().authorizeRequests().antMatchers("/index.html", "/home.html", "/login.html", "/").permitAll().anyRequest()
-          .authenticated();
-    }
   }
 
   @RequestMapping("/user")
