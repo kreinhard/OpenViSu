ef77812bfd3b9b012ecf5477994b7c906d4966cb
Kai Reinhard <K.Reinhard@micromata.de>
2016-04-03 17:10:07 +0200

    Moved.

--- src/test/java/org/openvisu/OpenvisuApplicationTests.java
+++ src/test/java/org/openvisu/OpenvisuApplicationTests.java
@@ -1,7 +1,8 @@
-package com.example;
+package org.openvisu;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.openvisu.OpenvisuApplication;
 import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.boot.test.SpringApplicationConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
