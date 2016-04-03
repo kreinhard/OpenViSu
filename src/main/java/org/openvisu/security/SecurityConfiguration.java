e944049837f33cd17ab80e5ca09c06bda82df1e8
Kai Reinhard <K.Reinhard@micromata.de>
2016-04-03 22:16:43 +0200

    Form login.

--- src/main/java/org/openvisu/security/SecurityConfiguration.java
+++ src/main/java/org/openvisu/security/SecurityConfiguration.java
@@ -23,8 +23,9 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter
   @Override
   protected void configure(HttpSecurity http) throws Exception
   {
-    http.httpBasic().and().authorizeRequests().antMatchers("/index.html", "/home.html", "/login.html", "/").permitAll().anyRequest()
-        .authenticated().and().csrf().csrfTokenRepository(csrfTokenRepository()).and().addFilterAfter(csrfHeaderFilter(), CsrfFilter.class);
+    http.formLogin().and().logout().and().authorizeRequests().antMatchers("/index.html", "/home.html", "/login.html", "/").permitAll()
+        .anyRequest().authenticated().and().csrf().csrfTokenRepository(csrfTokenRepository()).and()
+        .addFilterAfter(csrfHeaderFilter(), CsrfFilter.class);
   }
 
   private Filter csrfHeaderFilter()
