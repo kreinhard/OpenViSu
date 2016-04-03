c9cd77626c2274fe0d67f0530e5c91d5ddf807df
Kai Reinhard <K.Reinhard@micromata.de>
2016-04-03 17:21:17 +0200

    Angularjs works now.

--- src/main/resources/static/js/hello.js
+++ src/main/resources/static/js/hello.js
@@ -1,5 +1,7 @@
 angular.module('hello', [])
-  .controller('home', function() {
-    this.greeting = {id: 'xxx', content: 'Hello World!'}
-})
-
+  .controller('home', function($http) {
+  var self = this;
+  $http.get('/resource/').then(function(response) {
+    self.greeting = response.data;
+  })
+});
