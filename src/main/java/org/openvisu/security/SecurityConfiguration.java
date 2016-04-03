1aa44068297590f984fa64ba967c805318013d2c
Kai Reinhard <K.Reinhard@micromata.de>
2016-04-03 23:08:41 +0200

    Login works.

--- src/main/java/org/openvisu/security/SecurityConfiguration.java
+++ src/main/java/org/openvisu/security/SecurityConfiguration.java
@@ -1,6 +1,8 @@
 package org.openvisu.security;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.servlet.Filter;
 import javax.servlet.FilterChain;
@@ -9,8 +11,16 @@ import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.web.csrf.CsrfFilter;
 import org.springframework.security.web.csrf.CsrfToken;
 import org.springframework.security.web.csrf.CsrfTokenRepository;
@@ -23,9 +33,37 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter
   @Override
   protected void configure(HttpSecurity http) throws Exception
   {
-    http.formLogin().and().logout().and().authorizeRequests().antMatchers("/index.html", "/home.html", "/login.html", "/").permitAll()
-        .anyRequest().authenticated().and().csrf().csrfTokenRepository(csrfTokenRepository()).and()
-        .addFilterAfter(csrfHeaderFilter(), CsrfFilter.class);
+    http.formLogin().loginPage("/login").permitAll().and().logout().and().authorizeRequests()
+        .antMatchers("/index.html", "/home.html", "/login.html", "/").permitAll().anyRequest().authenticated().and().csrf()
+        .csrfTokenRepository(csrfTokenRepository()).and().addFilterAfter(csrfHeaderFilter(), CsrfFilter.class);
+  }
+
+  @Autowired
+  public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception
+  {
+    auth.authenticationProvider(new AuthenticationProvider() {
+
+      @Override
+      public Authentication authenticate(Authentication authentication) throws AuthenticationException
+      {
+        String name = authentication.getName();
+        String password = authentication.getCredentials().toString();
+
+        // use the credentials to try to authenticate against the third party system
+        if ("user".equals(name) && "pw".equals(password)) {
+          List<GrantedAuthority> grantedAuths = new ArrayList<>();
+          return new UsernamePasswordAuthenticationToken(name, password, grantedAuths);
+        } else {
+          throw new BadCredentialsException("Unable to auth against third party systems");
+        }
+      }
+
+      @Override
+      public boolean supports(Class< ? > authentication)
+      {
+        return authentication.equals(UsernamePasswordAuthenticationToken.class);
+      }
+    });
   }
 
   private Filter csrfHeaderFilter()
--- src/main/resources/static/js/hello.js
+++ src/main/resources/static/js/hello.js
@@ -1,4 +1,4 @@
-angular.module('hello', [ 'ngRoute' ]).config(function($routeProvider, $httpProvider) {
+angular.module('hello', [ 'ngRoute' ]).config(function($routeProvider) {
 
 	$routeProvider.when('/', {
 		templateUrl : 'home.html',
@@ -10,64 +10,72 @@ angular.module('hello', [ 'ngRoute' ]).config(function($routeProvider, $httpProv
 		controllerAs: 'controller'
 	}).otherwise('/');
 
-	$httpProvider.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
-
 }).controller('navigation',
 
-		function($rootScope, $http, $location, $route) {
-			
-			var self = this;
-
-			self.tab = function(route) {
-				return $route.current && route === $route.current.controller;
-			};
-
-			var authenticate = function(credentials, callback) {
-
-				var headers = credentials ? {
-					authorization : "Basic "
-							+ btoa(credentials.username + ":"
-									+ credentials.password)
-				} : {};
-
-				$http.get('user', {
-					headers : headers
-				}).then(function(response) {
-					if (response.data.name) {
-						$rootScope.authenticated = true;
-					} else {
-						$rootScope.authenticated = false;
-					}
-					callback && callback($rootScope.authenticated);
-				}, function() {
-					$rootScope.authenticated = false;
-					callback && callback(false);
-				});
-
-			}
-
-			authenticate();
-
-			self.credentials = {};
-			self.login = function() {
-				authenticate(self.credentials, function(authenticated) {
-					if (authenticated) {
-						console.log("Login succeeded")
-						$location.path("/");
-						self.error = false;
-						$rootScope.authenticated = true;
-					} else {
-						console.log("Login failed")
-						$location.path("/login");
-						self.error = true;
-						$rootScope.authenticated = false;
-					}
-				})
-			};
-
-		}).controller('home', function($http) {
+function($rootScope, $http, $location, $route) {
+	
 	var self = this;
-	$http.get('/resource/').then(function(response) {
+
+	self.tab = function(route) {
+		return $route.current && route === $route.current.controller;
+	};
+
+	var authenticate = function(callback) {
+
+		$http.get('user').then(function(response) {
+			if (response.data.name) {
+				$rootScope.authenticated = true;
+			} else {
+				$rootScope.authenticated = false;
+			}
+			callback && callback();
+		}, function() {
+			$rootScope.authenticated = false;
+			callback && callback();
+		});
+
+	}
+
+	authenticate();
+
+	self.credentials = {};
+	self.login = function() {
+		$http.post('login', $.param(self.credentials), {
+			headers : {
+				"content-type" : "application/x-www-form-urlencoded"
+			}
+		}).then(function() {
+			authenticate(function() {
+				if ($rootScope.authenticated) {
+					console.log("Login succeeded")
+					$location.path("/");
+					self.error = false;
+					$rootScope.authenticated = true;
+				} else {
+					console.log("Login failed with redirect")
+					$location.path("/login");
+					self.error = true;
+					$rootScope.authenticated = false;
+				}
+			});
+		}, function() {
+			console.log("Login failed")
+			$location.path("/login");
+			self.error = true;
+			$rootScope.authenticated = false;
+		})
+	};
+
+	self.logout = function() {
+		$http.post('logout', {}).finally(function() {
+			$rootScope.authenticated = false;
+			$location.path("/");
+		});
+	}
+
+}).controller('home', function($http) {
+	var self = this;
+	$http.get('http://localhost:9000').then(function(response) {
 		self.greeting = response.data;
 	})
 });
