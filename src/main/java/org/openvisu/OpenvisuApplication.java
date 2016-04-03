323928b1591a2c618b66107b61d629c623e35599
Kai Reinhard <K.Reinhard@micromata.de>
2016-04-03 17:47:39 +0200

    Navigation, dummy login.

--- src/main/java/org/openvisu/OpenvisuApplication.java
+++ src/main/java/org/openvisu/OpenvisuApplication.java
@@ -1,11 +1,17 @@
 package org.openvisu;
 
+import java.security.Principal;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.UUID;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.SecurityProperties;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,14 +19,33 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class OpenvisuApplication
 {
+  @Configuration
+  @Order(SecurityProperties.ACCESS_OVERRIDE_ORDER)
+  protected static class SecurityConfiguration extends WebSecurityConfigurerAdapter
+  {
+    @Override
+    protected void configure(HttpSecurity http) throws Exception
+    {
+      http.httpBasic().and().authorizeRequests().antMatchers("/index.html", "/home.html", "/login.html", "/").permitAll().anyRequest()
+          .authenticated();
+    }
+  }
+
+  @RequestMapping("/user")
+  public Principal user(Principal user)
+  {
+    return user;
+  }
+
   @RequestMapping("/resource")
-  public Map<String,Object> home() {
-    Map<String,Object> model = new HashMap<String,Object>();
+  public Map<String, Object> home()
+  {
+    Map<String, Object> model = new HashMap<String, Object>();
     model.put("id", UUID.randomUUID().toString());
     model.put("content", "Hello World!");
     return model;
   }
-  
+
   public static void main(String[] args)
   {
     SpringApplication.run(OpenvisuApplication.class, args);
