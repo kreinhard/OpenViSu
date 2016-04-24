48c8e55a52e990c2b309395e486c4cd8715b07cc
Kai Reinhard <K.Reinhard@micromata.de>
2016-04-24 09:06:28 +0200

    Example: http://brewhouse.io/blog/2014/12/09/authentication-made-simple-in-single-page-angularjs-applications.html (doesn't work).

--- src/main/resources/static/js/app.js
+++ src/main/resources/static/js/app.js
@@ -27,10 +27,48 @@
                     requireLogin: true // this property will apply to all children of 'app'
                 }
             })
+
         // .state('app.playEvent', {
         // child state of `app`
         // requireLogin === true
         //});
+    });
+
+    application.config(function ($httpProvider) {
+
+        $httpProvider.interceptors.push(function ($timeout, $q, $injector) {
+            var loginModal, $http, $state;
+
+            // this trick must be done so that we don't receive
+            // `Uncaught Error: [$injector:cdep] Circular dependency found`
+            $timeout(function () {
+                loginModal = $injector.get('loginModal');
+                $http = $injector.get('$http');
+                $state = $injector.get('$state');
+            });
+
+            return {
+                responseError: function (rejection) {
+                    if (rejection.status !== 401) {
+                        return $q.reject(rejection);
+                    }
+
+                    var deferred = $q.defer();
+
+                    loginModal()
+                        .then(function () {
+                            deferred.resolve($http(rejection.config));
+                        })
+                        .catch(function () {
+                            $state.go('welcome');
+                            deferred.reject(rejection);
+                        });
+
+                    return deferred.promise;
+                }
+            };
+        });
+
     });
 
     application.config(['$provide', '$stateProvider', '$urlRouterProvider', '$httpProvider',
